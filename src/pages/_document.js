c01a1a84e4bb76dade48e6e4d9877dbff6620059
kedarlin <kedarkamaf@gmail.com>
2024-05-24 10:44:56 +0530

    inited

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,11 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link rel="preload" href="/fonts/Roadradio-Thin.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
-        <link rel="preload" href="/fonts/Roadradio-Bold.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
-        <link rel="preload" href="/fonts/ShareTechMono-Regular.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
